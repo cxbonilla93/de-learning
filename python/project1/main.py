2ab47c95a88db4ee8924ac0e403bc9e5c62099b2
Carlos Xavier Bonilla <cxbonilla@gmail.com>
2025-04-14 20:22:01 -0400

    Initial python scripts
    Initial python scripts

---
+++ python/project1/main.py
@@ -0,0 +1,13 @@
+string = "hello world" # create variable
+integer = 4 # whole number
+float_ = float(3.14)
+variable_of_interest = float_
+print(variable_of_interest, type(variable_of_interest))
+
+float(4)
+
+boolean = 3 < 2
+print(boolean)
+
+operation = 12/4
+print(operation)
